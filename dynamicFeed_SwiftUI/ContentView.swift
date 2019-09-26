6d268acd468678ee95a5cfbf06168468eee5300e
Max Gilespie <max.gillespie10@gmail.com>
2019-09-26 15:16:01 -0500

    first commit

---
+++ dynamicFeed_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dynamicFeed_SwiftUI
+//
+//  Created by Max Gillespie on 9/26/19.
+//  Copyright © 2019 Max Gillespie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
